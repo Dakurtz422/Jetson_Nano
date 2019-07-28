9648e3c4755735cde6c955614e3069dd0d2ecc99
Dakurtz422 <53398742+Dakurtz422@users.noreply.github.com>
2019-07-28 20:48:38 +0900

    Jetson Nano Temperature Check
    Temperature Check script for Jetson nano - extensible with email notification program, os-shutdown etc..

---
+++ nano_temp.py
@@ -0,0 +1,31 @@
+import subprocess
+from time import sleep
+import datetime
+
+# Script for checking a temperature of Jetson Nano cores
+# Set your values here
+max_temp = 50
+sleep_time = 30
+
+
+def temp():
+	process = subprocess.run('cat /sys/devices/virtual/thermal/thermal_zone*/temp', shell=True, capture_output=True)
+	temp_out = list((process.stdout.decode()).split("\n"))
+	# Delete last one (none) and -2 (always 100°C)
+	del temp_out[-1]
+	del temp_out[-2]
+	#print(temp_out)
+
+	for y, x in enumerate(temp_out):
+		x = int(x) / 1000
+		time = datetime.datetime.now()
+		time = time.strftime(" -> /%m/%d  %H:%M")
+		print(x ,"°C" , time)
+		if x > max_temp:
+			# If HOT do something here (send mails, etc..)
+			print(f"Processor {y} is HOT" )
+
+while True:
+	temp()
+	sleep(sleep_time)
+	print("")
